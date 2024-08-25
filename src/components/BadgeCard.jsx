802c11e79867698eccf7a157b0c931ec8fcf34e9
the0choi <theodore.choi07@gmail.com>
2024-08-25 17:04:07 +1000

    add progressive system section

---
+++ src/components/BadgeCard.jsx
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+
+
+const BadgeCard = ({ badgeImage, badgeStyle, badgeName, textColor, backgroundColor, icon, textContent, semicircleGradient, circleGradient, bottomGradient, current }) => {
+  return (
+    <div className="sm:w-[130px] xl:w-[170px] flex flex-col items-start">
+      <div className={`relative sm:h-[150px] xl:h-[215px] flex flex-col justify-end items-center xl:gap-[20px] self-stretch xl:pt-[34px] rounded-[6px] ${backgroundColor}`}>
+        {current && (
+          <div className="absolute right-0 top-0 h-[20px] px-[6px] py-[3px] border border-1 flex justify-center items-center gap-[4px] rounded-[4px] border-[#4A4E6B] bg-[radial-gradient(106.76%_124.95%_at_77.08%_-35.83%,_#3D4164_32.6%,_#353950_100%)] ">
+            <span className="text-[#9A9EC8] text-[12px] font-bold leading-[15px] tracking-[0.1px]">
+              Current
+            </span>
+          </div>
+        )}
+        <div className="sm:w-[130px] xl:w-[170px] sm:h-[42px] xl:h-[68.331px] absolute left-0 top-0 sm:translate-y-[54px] xl:translate-y-[73px]">
+          {semicircleGradient}
+        </div>
+        <div className="relative w-[54px] xl:w-[86px] h-[54px] xl:h-[86px] shrink-0">
+          {circleGradient}
+          <img src={badgeImage} className={`shrink-0 z-10 absolute inset-0 ${badgeStyle}`} />
+        </div>
+
+        <div className={`h-[74.7px] pb-[20px] xl:pb-[34px] flex flex-col justify-end items-center gap-[2px] xl:gap-[4px] shrink-0 self-stretch ${bottomGradient}`}>
+          <span className="text-[#FFF] text-[14px] font-bold leading-[18px] tracking-[0.1px] z-10">
+            {badgeName}
+          </span>
+
+          <div className="w-[73.7px] h-[18.7px] flex justify-center items-center gap-[4px] shrink-0">
+            <div className="w-[12px] h-[12px] shrink-0">
+              {icon}
+            </div>
+            <span className="text-[12px] font-bold leading-[15px] tracking-[0.1px]" style={{ color: textColor }}>
+              {textContent}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+BadgeCard.propTypes = {
+  badgeImage: PropTypes.string.isRequired,
+  badgeStyle: PropTypes.string.isRequired,
+  badgeName: PropTypes.string.isRequired,
+  textColor: PropTypes.string.isRequired,
+  textContent: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string.isRequired,
+  icon: PropTypes.node,
+  semicircleGradient: PropTypes.node,
+  circleGradient: PropTypes.node,
+  bottomGradient: PropTypes.node,
+  current: PropTypes.bool
+};
+
+
+export default BadgeCard;
--- src/components/ProgressiveAffiliateSystem.jsx
+++ src/components/ProgressiveAffiliateSystem.jsx
@@ -1,13 +1,252 @@
+import ironBadge from '../assets/images/ironBadge.png';
+import copperBadge from '../assets/images/copperBadge.png';
+import titaniumBadge from '../assets/images/titaniumBadge.png';
+import opalBadge from '../assets/images/opalBadge.png';
+import amberBadge from '../assets/images/amberBadge.png';
 
-
+import BadgeCard from './BadgeCard';
 
 const ProgressiveAffiliateSystem = () => {
   return (
-    <div className="flex flex-col items-center gap-[20px] self-stretch py-[56px] bg-[linear-gradient(270deg,_#151628_0%,_#171A31_50%,_#15172B_100%)]">
-      <span className="text-[#FFF] text-center text-[24px] font-bold leading-[31px] tracking-[0.1px] self-stretch">
-        Progressive Affiliate System
-      </span>
-      
+    <div className="flex flex-col items-center sm:gap-[16px] xl:gap-[20px] self-stretch sm:py-[32px] xl:py-[56px] bg-[linear-gradient(270deg,_#151628_0%,_#171A31_50%,_#15172B_100%)]">
+      <div className="flex flex-col items-center sm:gap-[4px] xl:gap-[8px]">
+        <span className="text-[#FFF] text-center sm:text-[18px] xl:text-[24px] font-bold sm:leading-[23px] xl:leading-[31px] tracking-[0.1px] self-stretch">
+          Progressive Affiliate System
+        </span>
+        <span className="w-[500px] text-[#6A6FA3] text-center sm:text-[11px] xl:text-[13px] font-bold sm:leading-[14px] xl:leading-[17px] tracking-[0.1px] self-stretch">
+          The more users you refer the greater the rewards. Ascend through the ranks
+          to get up to 5% of every deposit and unlock exclusive features and rewards.
+        </span>
+      </div>
+
+      <div className="flex items-center gap-[12px]">
+
+        <BadgeCard 
+          badgeImage={ironBadge}
+          badgeStyle="w-[20px] xl:w-[30px] h-[22px] xl:h-[33px] translate-x-[17px] xl:translate-x-[28px] translate-y-[16px] xl:translate-y-[26.5px]" 
+          badgeName="IRON" 
+          textColor="#A3B1B4"
+          textContent="Unlocked"
+          backgroundColor="bg-[radial-gradient(78.62%_100%_at_50.04%_0%,_#32344C_0%,_#21233E_100%)]"
+          bottomGradient="bg-[linear-gradient(180deg,_#2B2D46_0%,_#20223D_100%)]"
+          icon={
+            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
+              <g clipPath="url(#clip0_4_35260)">
+                <path d="M5.5 0.650024C2.74302 0.650024 0.5 2.89298 0.5 5.65C0.5 8.40702 2.74302 10.65 5.5 10.65C8.25698 10.65 10.5 8.40702 10.5 5.65C10.5 2.89298 8.25704 0.650024 5.5 0.650024ZM8.37109 4.80546L5.22984 7.94669C5.09628 8.08025 4.91873 8.15377 4.72986 8.15377C4.54099 8.15377 4.36344 8.08025 4.22987 7.94669L2.62891 6.34574C2.49535 6.21217 2.42178 6.03462 2.42178 5.84575C2.42178 5.65683 2.49535 5.47929 2.62891 5.34572C2.76243 5.21216 2.93997 5.13859 3.1289 5.13859C3.31777 5.13859 3.49537 5.21216 3.62888 5.34577L4.72981 6.44664L7.37102 3.80544C7.50458 3.67188 7.68213 3.59837 7.871 3.59837C8.05987 3.59837 8.23742 3.67188 8.37098 3.80544C8.64678 4.08124 8.64678 4.52977 8.37109 4.80546Z" fill="#A3B1B4"/>
+              </g>
+              <defs>
+                <clipPath id="clip0_4_35260">
+                  <rect width="10" height="10" fill="white" transform="translate(0.5 0.650024)"/>
+                </clipPath>
+              </defs>
+            </svg>
+          }
+          semicircleGradient={
+            <svg xmlns="http://www.w3.org/2000/svg" width="170" height="69" viewBox="0 0 170 69" fill="none" className="w-full h-full" preserveAspectRatio="none">
+              <path d="M84.5 0C49.2809 0 23.3455 11.0378 0 28.9475V68L170 68.331V27.4108C146.9 10.6045 118.478 0 84.5 0Z" fill="url(#paint0_linear_4_35214)"/>
+              <defs>
+                <linearGradient id="paint0_linear_4_35214" x1="85" y1="0" x2="85" y2="68.331" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#363954"/>
+                  <stop offset="1" stopColor="#2B2D46"/>
+                </linearGradient>
+              </defs>
+            </svg>
+          }
+          circleGradient={
+            <svg xmlns="http://www.w3.org/2000/svg" width="86" height="87" viewBox="0 0 86 87" fill="none" className="w-full h-full">
+              <circle cx="43" cy="43.3" r="43" fill="url(#paint0_linear_4_35218)"/>
+              <defs>
+                <linearGradient id="paint0_linear_4_35218" x1="43" y1="0.300049" x2="43" y2="86.3" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#2C3054"/>
+                  <stop offset="1" stopColor="#464A62"/>
+                </linearGradient>
+              </defs>
+            </svg>
+          }
+          current={false}
+        />
+
+        <BadgeCard 
+          badgeImage={copperBadge}
+          badgeStyle="w-[30px] xl:w-[44px] h-[23px] xl:h-[34px] translate-x-[12px] xl:translate-x-[21px] translate-y-[15.5px] xl:translate-y-[26px]"  
+          badgeName="COPPER" 
+          textColor="#C98152"
+          textContent="Unlocked"
+          backgroundColor="bg-[radial-gradient(78.62%_100%_at_50.04%_0%,_#322D41_0%,_#21223A_100%)]"
+          bottomGradient="bg-[linear-gradient(180deg,_#2B263C_0%,_#20213C_100%)]"
+          icon={
+            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
+              <g clipPath="url(#clip0_4_35919)">
+                <path d="M5.5 0.650024C2.74302 0.650024 0.5 2.89298 0.5 5.65C0.5 8.40702 2.74302 10.65 5.5 10.65C8.25698 10.65 10.5 8.40702 10.5 5.65C10.5 2.89298 8.25704 0.650024 5.5 0.650024ZM8.37109 4.80546L5.22984 7.94669C5.09628 8.08025 4.91873 8.15377 4.72986 8.15377C4.54099 8.15377 4.36344 8.08025 4.22987 7.94669L2.62891 6.34574C2.49535 6.21217 2.42178 6.03462 2.42178 5.84575C2.42178 5.65683 2.49535 5.47929 2.62891 5.34572C2.76243 5.21216 2.93997 5.13859 3.1289 5.13859C3.31777 5.13859 3.49537 5.21216 3.62888 5.34577L4.72981 6.44664L7.37102 3.80544C7.50458 3.67188 7.68213 3.59837 7.871 3.59837C8.05987 3.59837 8.23742 3.67188 8.37098 3.80544C8.64678 4.08124 8.64678 4.52977 8.37109 4.80546Z" fill="#C98152"/>
+              </g>
+              <defs>
+                <clipPath id="clip0_4_35919">
+                  <rect width="10" height="10" fill="white" transform="translate(0.5 0.650024)"/>
+                </clipPath>
+              </defs>
+            </svg>
+          }
+          semicircleGradient={
+            <svg xmlns="http://www.w3.org/2000/svg" width="170" height="69" viewBox="0 0 170 69" fill="none" className="w-full h-full" preserveAspectRatio="none">
+              <path d="M84.5 0C49.2809 0 23.3455 11.0378 0 28.9475V68L170 68.331V27.4108C146.9 10.6045 118.478 0 84.5 0Z" fill="url(#paint0_linear_4_35879)"/>
+              <defs>
+                <linearGradient id="paint0_linear_4_35879" x1="85" y1="0" x2="85" y2="68.331" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#382E44"/>
+                  <stop offset="1" stopColor="#2B263C"/>
+                </linearGradient>
+              </defs>
+            </svg>
+          }
+          circleGradient={
+            <svg xmlns="http://www.w3.org/2000/svg" width="86" height="87" viewBox="0 0 86 87" fill="none" className="w-full h-full">
+              <circle cx="43" cy="43.3" r="43" fill="url(#paint0_linear_4_35883)"/>
+              <defs>
+                <linearGradient id="paint0_linear_4_35883" x1="43" y1="0.300049" x2="43" y2="86.3" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#2D2F52"/>
+                  <stop offset="1" stopColor="#4F3544"/>
+                </linearGradient>
+              </defs>
+            </svg>
+          }
+          current={false}
+        />
+
+        <BadgeCard 
+          badgeImage={titaniumBadge}
+          badgeStyle="w-[32px] xl:w-[48px] h-[28px] xl:h-[42px] translate-x-[11px] xl:translate-x-[19px] translate-y-[13px] xl:translate-y-[22px]"  
+          badgeName="TITANIUM" 
+          textColor="#A3B1B4"
+          textContent="Unlocked"
+          backgroundColor="bg-[radial-gradient(78.62%_100%_at_50.04%_0%,_#32344C_0%,_#21233E_100%)]"
+          bottomGradient="bg-[linear-gradient(180deg,_#2B2D46_0%,_#20223D_100%)]"
+          icon={
+            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
+              <g clipPath="url(#clip0_4_7594)">
+                <path d="M5.5 0.650024C2.74302 0.650024 0.5 2.89298 0.5 5.65C0.5 8.40702 2.74302 10.65 5.5 10.65C8.25698 10.65 10.5 8.40702 10.5 5.65C10.5 2.89298 8.25704 0.650024 5.5 0.650024ZM8.37109 4.80546L5.22984 7.94669C5.09628 8.08025 4.91873 8.15377 4.72986 8.15377C4.54099 8.15377 4.36344 8.08025 4.22987 7.94669L2.62891 6.34574C2.49535 6.21217 2.42178 6.03462 2.42178 5.84575C2.42178 5.65683 2.49535 5.47929 2.62891 5.34572C2.76243 5.21216 2.93997 5.13859 3.1289 5.13859C3.31777 5.13859 3.49537 5.21216 3.62888 5.34577L4.72981 6.44664L7.37102 3.80544C7.50458 3.67188 7.68213 3.59837 7.871 3.59837C8.05987 3.59837 8.23742 3.67188 8.37098 3.80544C8.64678 4.08124 8.64678 4.52977 8.37109 4.80546Z" fill="#A3B1B4"/>
+              </g>
+              <defs>
+                <clipPath id="clip0_4_7594">
+                  <rect width="10" height="10" fill="white" transform="translate(0.5 0.650024)"/>
+                </clipPath>
+              </defs>
+            </svg>
+          }
+          semicircleGradient={
+            <svg xmlns="http://www.w3.org/2000/svg" width="170" height="69" viewBox="0 0 170 69" fill="none" className="w-full h-full" preserveAspectRatio="none">
+              <path d="M84.5 0C49.2809 0 23.3455 11.0378 0 28.9475V68L170 68.331V27.4108C146.9 10.6045 118.478 0 84.5 0Z" fill="url(#paint0_linear_4_7542)"/>
+              <defs>
+                <linearGradient id="paint0_linear_4_7542" x1="85" y1="0" x2="85" y2="68.331" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#363954"/>
+                  <stop offset="1" stopColor="#2B2D46"/>
+                </linearGradient>
+              </defs>
+            </svg>
+          }
+          circleGradient={
+            <svg xmlns="http://www.w3.org/2000/svg" width="86" height="87" viewBox="0 0 86 87" fill="none" className="w-full h-full">
+              <circle cx="43" cy="43.3" r="43" fill="url(#paint0_linear_4_7546)"/>
+              <defs>
+                <linearGradient id="paint0_linear_4_7546" x1="43" y1="0.300049" x2="43" y2="86.3" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#2C3054"/>
+                  <stop offset="1" stopColor="#464A62"/>
+                </linearGradient>
+              </defs>
+            </svg>
+          }
+          current={false}
+        />
+
+        <BadgeCard 
+          badgeImage={opalBadge}
+          badgeStyle="w-[29.25px] xl:w-[45px] h-[27px] xl:h-[42px] translate-x-[12.4px] xl:translate-x-[20.5px] translate-y-[13.5px] xl:translate-y-[22px]"  
+          badgeName="OPAL" 
+          textColor="#CA83F5"
+          textContent="Unlocked"
+          backgroundColor="bg-[radial-gradient(78.62%_100%_at_50.04%_0%,_#312B51_0%,_#21213D_100%)]"
+          bottomGradient="bg-[linear-gradient(180deg,_#262547_0%,_#20223D_100%)]"
+          icon={
+            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
+              <g clipPath="url(#clip0_4_3140)">
+                <path d="M5.5 0.650024C2.74302 0.650024 0.5 2.89298 0.5 5.65C0.5 8.40702 2.74302 10.65 5.5 10.65C8.25698 10.65 10.5 8.40702 10.5 5.65C10.5 2.89298 8.25704 0.650024 5.5 0.650024ZM8.37109 4.80546L5.22984 7.94669C5.09628 8.08025 4.91873 8.15377 4.72986 8.15377C4.54099 8.15377 4.36344 8.08025 4.22987 7.94669L2.62891 6.34574C2.49535 6.21217 2.42178 6.03462 2.42178 5.84575C2.42178 5.65683 2.49535 5.47929 2.62891 5.34572C2.76243 5.21216 2.93997 5.13859 3.1289 5.13859C3.31777 5.13859 3.49537 5.21216 3.62888 5.34577L4.72981 6.44664L7.37102 3.80544C7.50458 3.67188 7.68213 3.59837 7.871 3.59837C8.05987 3.59837 8.23742 3.67188 8.37098 3.80544C8.64678 4.08124 8.64678 4.52977 8.37109 4.80546Z" fill="#CA83F5"/>
+              </g>
+              <defs>
+                <clipPath id="clip0_4_3140">
+                  <rect width="10" height="10" fill="white" transform="translate(0.5 0.650024)"/>
+                </clipPath>
+              </defs>
+            </svg>
+          }
+          semicircleGradient={
+            <svg xmlns="http://www.w3.org/2000/svg" width="170" height="69" viewBox="0 0 170 69" fill="none" className="w-full h-full" preserveAspectRatio="none">
+              <path d="M84.5 0C49.2809 0 23.3455 11.0378 0 28.9475V68L170 68.331V27.4108C146.9 10.6045 118.478 0 84.5 0Z" fill="url(#paint0_linear_4_3077)"/>
+              <defs>
+                <linearGradient id="paint0_linear_4_3077" x1="85" y1="0" x2="85" y2="68.331" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#372F5A"/>
+                  <stop offset="1" stopColor="#262547"/>
+                </linearGradient>
+              </defs>
+            </svg>
+          }
+          circleGradient={
+            <svg xmlns="http://www.w3.org/2000/svg" width="86" height="87" viewBox="0 0 86 87" fill="none" className="w-full h-full">
+              <circle cx="43" cy="43.3" r="43" fill="url(#paint0_linear_4_3081)"/>
+              <defs>
+                <linearGradient id="paint0_linear_4_3081" x1="43" y1="0.300049" x2="43" y2="86.3" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#2C2F54"/>
+                  <stop offset="1" stopColor="#44396B"/>
+                </linearGradient>
+              </defs>
+            </svg>
+          }
+          current={true}
+        />
+
+        <BadgeCard 
+          badgeImage={amberBadge}
+          badgeStyle="w-[32px] xl:w-[48px] h-[32px] xl:h-[48px] translate-x-[11px] xl:translate-x-[19px] translate-y-[11px] xl:translate-y-[19px]"  
+          badgeName="AMBER" 
+          textColor="#6A6FA3"
+          textContent="Locked"
+          backgroundColor="bg-[radial-gradient(78.62%_100%_at_50.04%_0%,_#37313B_0%,_#2A283B_100%)]"
+          bottomGradient="bg-[linear-gradient(180deg,_#2F2A3B_0%,_#20223C_100%)]"
+          icon={
+            <svg xmlns="http://www.w3.org/2000/svg" width="7" height="11" viewBox="0 0 7 11" fill="none">
+              <path fillRule="evenodd" clipRule="evenodd" d="M1.9163 4.59263C1.45857 4.6579 1.05118 4.74327 0.712187 4.82772V3.75723C0.712187 2.04391 1.96279 0.650024 3.5 0.650024C5.03721 0.650024 6.28781 2.04391 6.28781 3.75722V4.82772C5.94882 4.74327 5.54143 4.6579 5.0837 4.59263V3.75722C5.0837 2.78391 4.37325 1.99208 3.5 1.99208C2.62675 1.99208 1.9163 2.78391 1.9163 3.75722V4.59263ZM0 5.64305C0.471543 5.48117 1.84785 5.06379 3.50001 5.06379C5.14849 5.06379 6.52776 5.48144 7 5.6432V10.0708C6.52847 10.2326 5.15217 10.65 3.50001 10.65C1.85154 10.65 0.472261 10.2324 0 10.0706V5.64305ZM3.76286 9.11985V8.15836C4.10552 8.03449 4.35402 7.67549 4.354 7.25283C4.354 6.72799 3.9709 6.301 3.50001 6.301C3.02911 6.301 2.64601 6.72799 2.64601 7.25283C2.64601 7.67549 2.89452 8.03449 3.23715 8.15836V9.11985H3.76286Z" fill="#6A6FA3"/>
+            </svg>
+          }
+          semicircleGradient={
+            <svg xmlns="http://www.w3.org/2000/svg" width="170" height="69" viewBox="0 0 170 69" fill="none" className="w-full h-full" preserveAspectRatio="none">
+              <path d="M84.5 0C49.2809 0 23.3455 11.0378 0 28.9475V68L170 68.331V27.4108C146.9 10.6045 118.478 0 84.5 0Z" fill="url(#paint0_linear_4_24251)"/>
+              <defs>
+                <linearGradient id="paint0_linear_4_24251" x1="85" y1="0" x2="85" y2="68.331" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#3D3540"/>
+                  <stop offset="1" stopColor="#2F2A3B"/>
+                </linearGradient>
+              </defs>
+            </svg>
+          }
+          circleGradient={
+            <svg xmlns="http://www.w3.org/2000/svg" width="86" height="87" viewBox="0 0 86 87" fill="none" className="w-full h-full">
+              <circle cx="43" cy="43.3" r="43" fill="url(#paint0_linear_4_24255)"/>
+              <defs>
+                <linearGradient id="paint0_linear_4_24255" x1="43" y1="0.300049" x2="43" y2="86.3" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#2D3052"/>
+                  <stop offset="1" stopColor="#503E3E"/>
+                </linearGradient>
+              </defs>
+            </svg>
+          }
+          current={false}
+        />
+
+      </div>
+
+      <div className="w-full sm:w-auto flex h-[40px] items-center cursor-pointer">
+        <div className="w-full flex items-center justify-center px-[16px] py-[10px] gap-[6px] rounded-[4px] border border-1 border-[#664B33] bg-[radial-gradient(63.62%_115%_at_46.04%_0%,_#473C39_0%,_#473C39_0.01%,_#202139_100%)]">
+          <span className="text-[#FFB436] text-center text-[14px] leading-[18px] tracking-[0.1px] font-bold">View Tiers</span>
+        </div>
+      </div>
+
     </div>
   )
 }
